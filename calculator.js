5346af2df5883ea0f4c202998622f47bcf352a47
biroa <adam.biro@gmail.com>
2017-01-08 18:57:49 +0000

    separate test from the controller

--- calculator.js
+++ calculator.js
@@ -18,23 +18,3 @@ angular
 	};
 		
 })();
-
-/* Test Code */
-describe('calculator', function () {
-    
-      beforeEach(module('calculatorApp'));
-      var $controller;
-      beforeEach(inject(function(_$controller_){
-        $controller = _$controller_;
-      }));
-      describe('sum', function () {
-        it('1 + 1 should equal 2', function () {
-          var $scope = {};
-          var controller = $controller('CalculatorController', { $scope: $scope });
-          controller.x = 1;
-          controller.y = 2;
-          controller.sum();
-          expect(controller.z).toBe(3);
-        }); 
-      });
-    });
